7bd7ec15774dacc45ddaeae8809385b7c27115e6
Kat S <kat@ipmem.com>
2022-02-03 23:25:02 +0300

    answer

--- file1.py
+++ file1.py
@@ -1,1 +1,11 @@
 print ("Hello, world")
+answer = 42
+right_answer = 34
+wrong_answer = 23
+
+print('Rigth answer is:', right_answer, sep='', end='')
+print('Second line')
+
+
+a = input('Пожалуйста введите ваше имя:')
+print(a)
