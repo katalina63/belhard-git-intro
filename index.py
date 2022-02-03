f18362e97f73fc057f0573b7d34a0a72026f360f
Kat S <kat@ipmem.com>
2022-02-03 23:34:49 +0300

    .sum

---
+++ index.py
@@ -0,0 +1,26 @@
+from unittest import result
+
+
+a = 0x0123456789
+f = 1.2e5
+print(f)
+a = '1.2'
+print(type(a))
+a = float(a)
+print(type(a))
+
+string = r'\n\t'
+print(string)
+
+a = 1
+b = 2 
+print(f'Sum of {a} and {b} is {a+b}')
+
+
+n=6
+result[-n]==result[len(result)-n]
+print(result[2:9])
+
+
+
+
